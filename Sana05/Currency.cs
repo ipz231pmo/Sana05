8d4f7c3a9ae7a561ee765a3bf070c5a73498a6da
Maksim Penkov <ipz231_pmo@student.ztu.edu.ua>
2023-12-30 15:25:35 +0200

    Add class Currency

---
+++ Sana05/Currency.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sana05
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public decimal ExRate { get; set; }
+        public Currency() 
+        { 
+            Name = string.Empty;
+            ExRate = 0;
+        }
+        public Currency(Currency currency) 
+        {
+            Name = currency.Name;
+            ExRate = currency.ExRate;
+        }
+        public Currency(string name, decimal exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+        public Currency(string name) 
+        {
+            Name = name;
+            ExRate=0;
+        }
+    }
+}
