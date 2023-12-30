0943cfee6ef775ec4f1bcecc8f5ff46d40a1e644
Maksim Penkov <ipz231_pmo@student.ztu.edu.ua>
2023-12-30 15:54:23 +0200

    Add demo

--- Sana05/Program.cs
+++ Sana05/Program.cs
@@ -1,7 +1,10 @@
-﻿internal class Program
+﻿using Sana05;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Airplane fligt = new Airplane("Zhytomyr", "Kiyv", new MyDate(), new MyDate(2023, 12, 31));
+        Product product = new Product("Laprop", 1000, new Currency("USA", 38.76M), 10, 3.3f);
     }
 }
